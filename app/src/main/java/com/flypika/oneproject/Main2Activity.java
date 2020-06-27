ed5e61b85743fd6bc2cee1b763162ae86ef9683a
Timur Seisembayev <timur.seisembayev@gmail.com>
2020-06-27 15:56:42 +0600

    transition between activities

--- app/src/main/java/com/flypika/oneproject/Main2Activity.java
+++ app/src/main/java/com/flypika/oneproject/Main2Activity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
-    public void sendMessage(View view) {
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+    public void back(View view) {
+        Intent intent = new Intent(Main2Activity.this, MainActivity.class);
         startActivity(intent);
     }
 }
